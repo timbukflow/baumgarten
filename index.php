d1cb9f1f52b743b9dc33f2958e6e0ed82e5698a1
timbuk <ivoschwizer@gmail.com>
2019-05-01 17:32:41 +0200

    start

---
+++ index.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html class="no-js" lang="de-CH">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Baumgarten</title>
+    <meta name="robots" content="all">
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="Graf AG Totalunternehmung">
+    
+    <meta name="google-site-verification" content="...folgt..." />
+    
+    <meta property="og:type" content="business.business">
+    <meta property="og:title" content="adlerweg-wittenbach">
+    <meta property="og:url" content="http://adlerweg.sg">
+    <meta property="business:contact_data:street_address" content="Oedenhofstrasse 11A">
+    <meta property="business:contact_data:locality" content="Wittenbach">
+    <meta property="business:contact_data:region" content="9300">
+    <meta property="business:contact_data:postal_code" content="9300">
+    <meta property="business:contact_data:country_name" content="Switzerland">
+    
+    <meta name="description" content="An zentraler Lage, mitten im Dorfkern von Wittenbach am Adlerweg, entstehen 3 neue Mehrfamilienhäuser mit insgesamt 51 Mietwohnungen, bei denen das Leben und dessen Begegnungen ganz im Zentrum stehen.">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#A52340">
+    <meta name="msapplication-TileColor" content="#fbfbfb">
+    <meta name="theme-color" content="#ffffff">
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+</head>
+
+<body>
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+    <div class="hnr">
+        <h2>B1</h2>
+    </div>
+    <div class="hg">
+        <div class="hover h1">
+            <svg version="1.1" id="bulding" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 145.5 126" style="enable-background:new 0 0 145.5 126;" xml:space="preserve">
+                <polygon class="st1" points="83,87.5 145,38.5 62.5,0.5 0.5,49.5 "/>
+                <polygon class="st1" points="83,125.5 0.5,87.5 0.5,49.5 83,87.5 "/>
+                <polygon class="st1" points="83,125.5 145,76.5 145,38.5 83,87.5 "/>
+            </svg>
+            <p class="over hovertext-1">hello</p>
+        </div>
+        
+        <div class="hover h2">
+            <svg version="1.1" id="bulding-2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 145.5 126" style="enable-background:new 0 0 145.5 126;" xml:space="preserve">
+                <polygon class="st2" points="83,87.5 145,38.5 62.5,0.5 0.5,49.5 "/>
+                <polygon class="st2" points="83,125.5 0.5,87.5 0.5,49.5 83,87.5 "/>
+                <polygon class="st2" points="83,125.5 145,76.5 145,38.5 83,87.5 "/>
+            </svg>
+            <p class="over hovertext-2">hello</p>
+        </div>
+        <div class="hover h3">
+            <svg version="1.1" id="bulding-3"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 145.5 126" style="enable-background:new 0 0 145.5 126;" xml:space="preserve">
+                <polygon class="st3" points="83,87.5 145,38.5 62.5,0.5 0.5,49.5 "/>
+                <polygon class="st3" points="83,125.5 0.5,87.5 0.5,49.5 83,87.5 "/>
+                <polygon class="st3" points="83,125.5 145,76.5 145,38.5 83,87.5 "/>
+            </svg>
+            <p class="over hovertext-3">hello</p>
+        </div>
+    </div>
+    
+    <div>
+        <table>   
+            <tr>      
+                <th>Wohnung</th>
+                <th class="l">Fläche</th>
+            </tr>   
+            <tr class="trh1 h1">
+                <td><span><b>1</b> / 3,5er</span></td> 
+                <td class="l">89.50 m²</td>
+            </tr> 
+            <tr class="trh2 h2">
+                <td><span><b>2</b> / 4,5er</span></td> 
+                <td class="l">118.00 m²</td>
+            </tr>
+            <tr class="trh3 h3"> 
+                <td><span><b>3</b> / 2,5er</span></td> 
+                <td class="l">56.30 m²</td>
+            </tr>
+        </table>
+    </div>
+    
+    
+    
+ 
+  <script src="js/vendor/modernizr-3.7.1.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    
+$( ".h1" ).hover(function() {
+    $( ".trh1" ).toggleClass("trhover")
+    $( ".st1" ).toggleClass("hoo");
+    $( ".hovertext-1" ).toggleClass("fonton");
+});
+    
+$( ".h2" ).hover(function() {
+    $( ".trh2" ).toggleClass("trhover")
+    $( ".st2" ).toggleClass("hoo");
+    $( ".hovertext-2" ).toggleClass("fonton");
+});
+
+$( ".h3" ).hover(function() {
+    $( ".trh3" ).toggleClass("trhover")
+    $( ".st3" ).toggleClass("hoo");
+    $( ".hovertext-3" ).toggleClass("fonton");
+});
+  
+    
+});
