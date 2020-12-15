ea442b0d88ba8844872046b42580091b11a9d8ab
timbukflow <ivoschwizer@gmail.com>
2020-12-15 17:40:30 +0100

    up

--- wohnangebot.php
+++ wohnangebot.php
@@ -933,15 +933,15 @@
                     </div>
                 </td>
             </tr>
-            <tr class="ce2w">
+            <tr class="ce2w r">
                 <td>C.04</td>
                 <td>1.OG</td>
                 <td>4.5</td>
                 <td>130.60 m²</td>
                 <td>26.20 m²</td>
-                <td>2'100.-</td>
-                <td>250.-</td>
-                <td>2'350.-</td>
+                <td></td>
+                <td></td>
+                <td>reserviert</td>
                 <td class="planb">
                     <img class="plani" src="img/plan.svg" alt="plan icon" width="21" height="18">
                     <div class="plan">
@@ -1134,7 +1134,7 @@
                 <td class="downloadplan" colspan="7"><a href="downloads/baumgarten-grundrisse-haus-c-mietwohung.pdf" download="Baumgarten-Haus-C-Grundrisspläne"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> <strong>Haus C | Mietwohnungen</strong> Alle Grundrisse (PDF)</a></td>
             </tr>
             <tr>
-                <td colspan="9" class="data">Mietzins und Nebenkosten in CHF | m² gerundet<br>Aktualisiert per 26.11.2020 | <span class="linkw konditionen impressum" data-toggle="#konditionen">Konditionen</span></td>
+                <td colspan="9" class="data">Mietzins und Nebenkosten in CHF | m² gerundet<br>Aktualisiert per 15.12.2020 | <span class="linkw konditionen impressum" data-toggle="#konditionen">Konditionen</span></td>
             </tr>
         </table>
         <div id="konditionen">
